8a5d9f54984120b22c9c893c8cea79bfd82d9ebd
KDuyHero <dtrang1570@gmail.com>
2022-11-19 22:58:16 +0700

    create user

--- src/server.js
+++ src/server.js
@@ -7,6 +7,10 @@ import initWebRoute from "./route/web";
 //require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // set up view engine
 configViewEngine(app);
 
