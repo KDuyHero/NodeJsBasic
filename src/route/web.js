3e2d65f67cce94444d80dc18e6e6957429931961
KDuyHero <dtrang1570@gmail.com>
2022-11-24 22:53:56 +0700

    upload single file

--- src/route/web.js
+++ src/route/web.js
@@ -1,5 +1,34 @@
 import express from "express";
 import homeController from "../controller/homeController";
+import multer from "multer";
+import path from "path";
+
+var appRoot = require("app-root-path");
+const storage = multer.diskStorage({
+  destination: function (req, res, cb) {
+    cb(null, appRoot + "/src/public/image/");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+const imageFilter = function (req, file, cb) {
+  // Accept images only
+  if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+    req.fileValidationError = "Only image files are allowed!";
+    return cb(new Error("Only image files are allowed!"), false);
+  }
+  cb(null, true);
+};
+// 'profile_pic' is the name of our file input field in the HTML form
+let upload = multer({
+  storage: storage,
+  fileFilter: imageFilter,
+});
 
 let router = express.Router();
 
@@ -10,6 +39,12 @@ const initWebRoute = (app) => {
   router.post("/delete-user", homeController.deleteUser);
   router.get("/edit-user/:id", homeController.getEditPage);
   router.post("/update-user", homeController.postUpdateUser);
+  router.get("/upload", homeController.getUploadFilePage);
+  router.post(
+    "/upload-profile-pic",
+    upload.single("profile_pic"),
+    homeController.handleUploadFile
+  );
   router.get("/about", (req, res) => {
     res.send("About");
   });
